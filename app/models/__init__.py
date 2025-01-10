ca2a93854c7bbb503aa4d92d3c593a8e60fce389
MonaKecil <flowyourminds@gmail.com>
2025-01-11 01:32:45 +0700

    Implement context manager

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,69 +1,64 @@
-from models import get_connection
+from app.models.database import get_connection
 
 
 def initialize_structure():
-    print("Establishing connection to database...")
-    conn = get_connection()
-    print("Connection established.")
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        print("Establishing connection to database...")
+        print("Connection established.")
+        cursor = conn.cursor()
 
-    # Create the main table if not exists
-    print("Creating urls table...")
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS urls (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            original_url TEXT NOT NULL,
-            shortened_url TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP,
-            deleted_at TIMESTAMP
-        )
-        """
-    )
-    conn.commit()
-    print("Table created.")
-
-    # Create trigger to make sure that the shortened_url is unique on creation
-    print("Creating unique_shortened_url_on_create trigger...")
-    cursor.execute(
-        """
-        CREATE TRIGGER IF NOT EXISTS unique_shortened_url_on_create
-        BEFORE INSERT ON urls
-        FOR EACH ROW
-        BEGIN
-            SELECT RAISE(ABORT, 'Shortened URL already exists, please choose another one.')
-            WHERE EXISTS (
-                SELECT 1
-                FROM urls
-                WHERE shortened_url = NEW.shortened_url AND deleted_at IS NULL
+        # Create the main table if not exists
+        print("Creating urls table...")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS urls (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                original_url TEXT NOT NULL,
+                shortened_url TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP,
+                deleted_at TIMESTAMP
             )
-        """
-    )
-    conn.commit()
-    print("Trigger created.")
+            """)
+        conn.commit()
+        print("Table created.")
 
-    # Create trigger to make sure that the shortened_url is unique on update
-    print("Creating unique_shortened_url_on_update trigger...")
-    cursor.execute(
-        """
-        CREATE TRIGGER IF NOT EXISTS unique_shortened_url_on_update
-        BEFORE UPDATE ON urls
-        FOR EACH ROW
-        BEGIN
-            SELECT RAISE(ABORT, 'Shortened URL already exists, please choose another one.')
-            WHERE EXISTS (
-                SELECT 1
-                FROM urls
-                WHERE shortened_url = NEW.shortened_url
-                    AND deleted_at IS NULL
-                    AND id != NEW.id
-            )
-        """
-    )
-    conn.commit()
-    print("Trigger created.")
-    conn.close()
+        # Create trigger to make sure that the shortened_url is unique on creation
+        print("Creating unique_shortened_url_on_create trigger...")
+        cursor.execute("""
+            CREATE TRIGGER IF NOT EXISTS unique_shortened_url_on_create
+            BEFORE INSERT ON urls
+            FOR EACH ROW
+            BEGIN
+                SELECT RAISE(ABORT, 'Shortened URL already exists.')
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM urls
+                    WHERE shortened_url = NEW.shortened_url AND deleted_at IS NULL
+                );
+            END;
+            """)
+        conn.commit()
+        print("Trigger created.")
+
+        # Create trigger to make sure that the shortened_url is unique on update
+        print("Creating unique_shortened_url_on_update trigger...")
+        cursor.execute("""
+            CREATE TRIGGER IF NOT EXISTS unique_shortened_url_on_update
+            BEFORE UPDATE ON urls
+            FOR EACH ROW
+            BEGIN
+                SELECT RAISE(ABORT, 'Shortened URL already exists.')
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM urls
+                    WHERE shortened_url = NEW.shortened_url
+                        AND deleted_at IS NULL
+                        AND id != NEW.id
+                );
+            END;
+            """)
+        conn.commit()
+        print("Trigger created.")
 
 
 initialize_structure()
---
+++ app/models/database.py
@@ -0,0 +1,133 @@
+import sqlite3
+import string
+import random
+
+
+def main():
+    print("Hello from models.py")
+    print("You're not supposed to run this file directly.")
+
+
+def generate_random_shortened_url() -> str:
+    """
+    Generates a random alphanumeric string of length 10.
+    """
+    string = "".join(random.choice(string.ascii_letters + string.digits)
+                     for _ in range(10))
+    return string
+
+
+def get_connection():
+    """
+    Returns a connection object to the database.
+    rows are treated as dictionaries
+    """
+    conn = sqlite3.connect("data.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_new_entry(
+        original_url: str,
+        shortened_url: str | None = None
+):
+    if shortened_url is None:
+        shortened_url = generate_random_shortened_url()
+
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO urls (original_url, shortened_url)
+            VALUES (?, ?)
+            RETURNING *;
+            """, (original_url, shortened_url)
+        )
+        data = cursor.fetchone()
+        conn.commit()
+        return dict(data)
+
+
+def batch_create_new_entries(
+    original_urls: list[str],
+    shortened_urls: list[str] = None
+):
+    if shortened_urls is None:
+        shortened_urls = [generate_random_shortened_url()
+                          for _ in range(len(original_urls))]
+
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.executemany("""
+            INSERT INTO urls (original_url, shortened_url)
+            VALUES (?, ?)
+            RETURNING *;
+            """, zip(original_urls, shortened_urls)
+        )
+        data = cursor.fetchall()
+        conn.commit()
+        return dict(data)
+
+
+def get_entry_by_id(id: int):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT * FROM urls WHERE id = ?
+            AND deleted_at IS NULL
+            """, (id,)
+        )
+        return cursor.fetchone()
+
+
+def get_all_entries():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT * FROM urls
+            WHERE deleted_at IS NULL
+            """)
+        return cursor.fetchall()
+
+
+def update_entry(id: int, original_url: str, shortened_url: str):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            UPDATE urls
+            SET
+                original_url = ?,
+                shortened_url = ?
+                updated_at = CURRENT_TIMESTAMP
+            WHERE id = ?
+            AND deleted_at IS NULL
+            RETURNING *;
+            """, (original_url, shortened_url, id)
+        )
+        data = cursor.fetchone()
+        conn.commit()
+        return dict(data)
+
+
+def delete_entry(id: int):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            UPDATE urls
+            SET deleted_at = CURRENT_TIMESTAMP
+            WHERE id = ?
+            RETURNING *;
+            """, (id,)
+        )
+        data = cursor.fetchone()
+        conn.commit()
+        return dict(data)
+
+
+if __name__ == "__main__":
+    main()
