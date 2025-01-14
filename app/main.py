60f13b95e610d68f5f5693c8b782751242e71039
Mona-kecil <flowyourminds@gmail.com>
2025-01-14 12:16:16 +0700

    move idempotency, caching, and rate_limiting logic into middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import json
+import hashlib
+import base64
 from typing import Annotated
 
-from fastapi import FastAPI, status, HTTPException, Header, Request, Path
+from fastapi import FastAPI, status, Request, Path
 from fastapi.responses import JSONResponse
 import redis
 
@@ -13,6 +15,85 @@ redis_client = redis.Redis(host="127.0.0.1", port=6379, decode_responses=True)
 
 RATE_LIMIT_WINDOW = 30  # seconds
 RATE_LIMIT_COUNT = 3  # requests/window
+CACHE_TTL = 300  # seconds
+
+
+def generate_short_hash(*, input_string: str, length: int = 5) -> str:
+    """
+    Generate a SHA256 hash of the input string and encode it in base64
+    truncating it to the desired length.
+    """
+    hash_obj = hashlib.sha256(input_string.encode())
+    base64_hash = base64.urlsafe_b64encode(hash_obj.digest()).decode()
+    return base64_hash[:length]
+
+
+@app.middleware("http")
+async def cache_middleware(request: Request, call_next):
+    unique_key = request.state.unique_key
+    if unique_key is None:
+        return await call_next(request)
+
+    cached_response = redis_client.get(unique_key)
+    if cached_response is not None:
+        data: dict = json.loads(cached_response)
+        data['type'] = "cache"
+        print('fetch data from cache')
+        return JSONResponse(status_code=status.HTTP_201_CREATED,
+                            content=data,
+                            media_type="application/json")
+
+    response = await call_next(request)
+    return response
+
+
+@app.middleware("http")
+async def idempotency_middleware(request: Request, call_next):
+    original_url = str(request.url.path)
+    if request.url.query:
+        original_url += f"?{request.url.query}"
+
+    urls = original_url.split('?')
+    queries = urls[0].split('&')
+    print(urls)
+    print(queries)
+
+    key = f"{request.client.host}:{request.client.port}:{original_url}"
+    print(key)
+
+    unique_key = generate_short_hash(input_string=key)
+    if redis_client.exists(unique_key):
+        data = json.loads(redis_client.get(unique_key))
+        data["message"] = "Already shortened."
+        return JSONResponse(status_code=status.HTTP_201_CREATED,
+                            content=data,
+                            media_type="application/json")
+    request.state.unique_key = unique_key
+    response = await call_next(request)
+    return response
+
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next):
+    client_id = f"{request.client.host}:{request.client.port}"
+    key = f"rate_limit:{client_id}"
+
+    if redis_client.exists(key):
+        request_count = int(redis_client.get(key))
+    else:
+        redis_client.set(key, 0, ex=RATE_LIMIT_WINDOW)
+        request_count = 0
+
+    if request_count >= RATE_LIMIT_COUNT:
+        return JSONResponse(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            content="Rate limit exceeded. Try again later."
+        )
+
+    redis_client.incr(key)
+
+    response = await call_next(request)
+    return response
 
 
 @app.post("/shorten")
@@ -20,36 +101,18 @@ async def shorten_url(
     request: Request,
     url: str,
     shortened_url: str | None = None,
-    idempotency_key: str = Header(...)
 ):
-    cached_response = redis_client.get(idempotency_key)
-    if cached_response is not None:
-        return JSONResponse(status_code=status.HTTP_201_CREATED,
-                            content=json.loads(cached_response),
-                            media_type="application/json")
+    try:
+        shorten: dict = db.create_new_entry(url, shortened_url)
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT,
+                            content={"message": "Failed to shorten URL. Shortened URL already used."})
 
-    client_id = f"{request.client.host}:{request.client.port}"
-    key = f"rate_limit:{client_id}"
-
-    if redis_client.exists(key):
-        request_count = int(redis_client.get(key))
-    else:
-        request_count = int(redis_client.set(key, 1, ex=RATE_LIMIT_WINDOW))
-
-    if request_count >= RATE_LIMIT_COUNT:
-        raise HTTPException(
-            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-            detail="Rate limit exceeded. Try again later."
-        )
-
-    redis_client.incr(key)
-
-    shorten: dict = db.create_new_entry(url, shortened_url)
-    cache = shorten.copy()
-    cache["type"] = "cache"
-
-    redis_client.set(idempotency_key, json.dumps(
-        cache), ex=RATE_LIMIT_WINDOW)
+    unique_key = request.state.unique_key
+    redis_client.set(unique_key,
+                     json.dumps(shorten),
+                     ex=CACHE_TTL)
 
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
@@ -63,7 +126,14 @@ async def read_url(
     request: Request,
     shortened_url: Annotated[str, Path(title="Shortened URL")]
 ):
+    data = db.get_entry(shortened_url)
     return JSONResponse(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        content={"message": "Not implemented"},
+        status_code=status.HTTP_201_CREATED,
+        content=data,
+        media_type="application/json"
     )
+
+
+@app.get("/")
+async def read_redis():
+    return redis_client.scan(0)
